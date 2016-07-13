92108bd43c2d5b4feb46ffcd86b4f5c1f544f722
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-07-13 15:08:18 +0300

    version 0.2

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 'use strict';
 
 const debug = require('debug')('speed');
+const symbol = Symbol.for('since-app-start');
+const profilings = Object.create(null);
 
-const startTime = 'appStartTime' in global ?
-  global.appStartTime :
-  Date.now();
-const profilings = {};
-
-exports.sinceStart = () => Date.now() - startTime;
-
-exports.addEntry = (name) => {
-  const ms = exports.sinceStart();
-  profilings[name] = ms;
-  return name + ' ' + ms + 'ms';
+const sinceStart = () => {
+  if (!(symbol in global)) {
+    throw new ReferenceError('.start() should be called first');
+  }
+  return Date.now() - global[symbol];
 };
 
-exports.profile = (name) => debug(exports.addEntry(name));
+const addEntry = name => {
+  const ms = sinceStart();
+  profilings[name] = ms;
+  return `${name} ${ms} ms`;
+};
+
+module.exports = {
+  profilings,
+  sinceStart,
+  addEntry,
+  profile(name) {
+    return debug(addEntry(name));
+  },
+  start() {
+    if (symbol in global) return;
+    global[symbol] = Date.now();
+  }
+};
